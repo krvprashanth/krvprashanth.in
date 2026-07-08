6b3074e95350c66f9ea8c86906d2110d9aa37c41
Kurva Prashanth <krvprashanth@riseup.net>
2026-07-08 21:26:47 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -7,7 +7,7 @@ BLOG_AUTHOR = "krvprashanth"  # (translatable)
 BLOG_TITLE = "krvprashanth"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://krvprashanth.in/"
+SITE_URL = "https://krvprashanth.in"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://krvprashanth.in/"
